1a7ebbd282c4c1938af87f5d27624eae3434d807
Hanna <hannamaria446@gmail.com>
2019-07-31 23:30:44 -0500

    first four exercises

---
+++ array-reduce.js
@@ -0,0 +1,3 @@
+module.exports = function countWords(inputWords) {
+    
+  }
---
+++ every-some.js
@@ -0,0 +1,10 @@
+    
+module.exports = function checkUsersValid(goodUsers) {
+    return function(submittedUsers) {
+      return submittedUsers.every(function(submittedUser) {
+        return goodUsers.some(function(goodUser) {
+          return goodUser.id === submittedUser.id
+        })
+      })
+    }
+  }
